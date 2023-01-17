adbf42604f43c4028de22a5b7e89822ed7fe9d7e
James-Kelley-Galvanize <james.kelley@galvanize.com>
2023-01-17 10:17:18 -0700

    add knexfile

---
+++ knexfile.js
@@ -0,0 +1,49 @@
+const connectionString = process.env.DATABASE_URL; // USE SAME FOR ALL EXPERIMENTS TO KEEP IT EASY?
+
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      connectionString,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  production: {
+    client: "pg",
+    connection: {
+      connectionString,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+};
